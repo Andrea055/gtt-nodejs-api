cda0e3932db0e74b01ef364bfbea741ee626ab6e
Andrea Canale <vivanco11.ac@gmail.com>
2021-11-16 12:14:05 +0100

    Create example.js

---
+++ example.js
@@ -0,0 +1,24 @@
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const prompt = require("prompt-sync")({ sigint: true });
+let dati = []
+let nfermata=prompt("Inserisci una fermata:")
+fetch("https://gpa.madbob.org/query.php?stop=" + nfermata)
+.then(response => {
+  if (!response.ok) {
+    alert("C'è stato un errore durante la tua richiesta")
+    throw new Error(`Request failed with status ${response.status}`)
+  }
+  return response.json()
+})
+.then(data => {
+  let dati = []
+  let long= Object.keys(data).length
+  console.log("Lunghezza:" + long)
+  for (let n = 0; n < long; n++) {
+    dati.push(data[n].line)
+  }
+
+  console.log(dati[9]);
+})
+.catch(error => console.log(error))
+ 
